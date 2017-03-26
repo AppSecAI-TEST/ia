9df379a532544baed07175c81753cb0910ee5fd0
lucas <lucasdcampaner@gmail.com>
2017-03-26 16:31:41 -0300

    Informa que una posicion esta ocupada.

--- cuatroEnLinea/src/main/java/ar/edu/untref/ia/Tablero.java
+++ cuatroEnLinea/src/main/java/ar/edu/untref/ia/Tablero.java
@@ -4,7 +4,19 @@ public class Tablero {
 
 	private String [][] posiciones = new String[7][6]; 
 			 
+
+	public void setPosicion(int i, int j, String ficha) {
+		this.posiciones[i][j] = ficha;
+	}
+	
 	public String getPosicion(int i, int j) {
 		return this.posiciones[i][j];
 	}
+
+	public String [][] getPosiciones() {
+		return this.posiciones;
+	}
+
+
+
 }
--- cuatroEnLinea/src/test/java/ar/edu/untref/ia/TableroTest.java
+++ cuatroEnLinea/src/test/java/ar/edu/untref/ia/TableroTest.java
@@ -29,16 +29,31 @@ public class TableroTest {
 
 	@Test
 	public void elTableroInformaQueUnaPosicionCualquieraEstaLibre() {
-		
+
 		boolean posicionLibre = true;
-		Tablero tablero = new Tablero();		
-		
+		Tablero tablero = new Tablero();
+
 		String posicion = tablero.getPosicion(1, 3);
 		if (posicion != null) {
 			posicionLibre = false;
 		}
-		
+
 		Assert.assertTrue(posicionLibre);
 	}
 
+	@Test
+	public void elTableroInformaQueUnaPosicionEstaOcupada() {
+
+		boolean posicionLibre = true;
+		Tablero tablero = new Tablero();
+		tablero.setPosicion(1, 3, "X");
+
+		String posicion = tablero.getPosicion(1, 3);
+		if (posicion != null) {
+			posicionLibre = false;
+		}
+
+		Assert.assertTrue(!posicionLibre);
+		Assert.assertTrue(tablero.getPosicion(1, 3) == "X");
+	}
 }
