758d404b8040d6a02c4d192444be248ea12cb095
lucas <lucasdcampaner@gmail.com>
2017-03-26 16:58:08 -0300

    Comprueba la primer jugada de la partida.

--- cuatroEnLinea/src/main/java/ar/edu/untref/ia/Tablero.java
+++ cuatroEnLinea/src/main/java/ar/edu/untref/ia/Tablero.java
@@ -2,21 +2,61 @@ package ar.edu.untref.ia;
 
 public class Tablero {
 
-	private String [][] posiciones = new String[7][6]; 
-			 
+	private final int JUGADOR_UNO = 1;
+	private final String FICHA_JUGADOR_UNO = "O";
+	private final int JUGADOR_DOS = 2;
+	private final String FICHA_JUGADOR_DOS = "X";
+	
+	private String[][] posiciones = new String[7][6];
 
 	public void setPosicion(int i, int j, String ficha) {
 		this.posiciones[i][j] = ficha;
 	}
-	
+
 	public String getPosicion(int i, int j) {
 		return this.posiciones[i][j];
 	}
 
-	public String [][] getPosiciones() {
+	public String[][] getPosiciones() {
 		return this.posiciones;
 	}
 
+	public boolean jugar(int jugador, int columna) {
 
+		boolean jugadaValida = hayEspacioEnLaColumna(columna);
+		int indiceColumna = columna - 1;
+		
+		if (jugadaValida) {
+		
+			int filaLibre = obtenerFilaLibre(indiceColumna);
+			
+			if (jugador == JUGADOR_UNO) {
+				this.posiciones[filaLibre][indiceColumna] = FICHA_JUGADOR_UNO;
+			}
+	
+			if (jugador == JUGADOR_DOS) {
+				this.posiciones[filaLibre][indiceColumna] = FICHA_JUGADOR_DOS;
+			}
+		}
+		return jugadaValida;
+	}
+	
+	private int obtenerFilaLibre(int columna) {
+		
+		boolean filaEncontrada = false;
+		int filaLibre = 0;
+		
+		while (filaLibre < 6 && !filaEncontrada) {
+			if (this.getPosicion(filaLibre, columna) != null) {
+				filaEncontrada = true;
+			}
+			filaLibre++;
+		}
+		
+		return filaLibre - 1;
+	}
 
+	private boolean hayEspacioEnLaColumna(int columna) {
+		return (getPosicion(0, columna) == null);
+	}
 }
--- cuatroEnLinea/src/test/java/ar/edu/untref/ia/TableroTest.java
+++ cuatroEnLinea/src/test/java/ar/edu/untref/ia/TableroTest.java
@@ -5,6 +5,11 @@ import org.junit.Test;
 
 public class TableroTest {
 
+	private final int JUGADOR_UNO = 1;
+	private final String FICHA_JUGADOR_UNO = "O";
+	private final int JUGADOR_DOS = 2;
+	private final String FICHA_JUGADOR_DOS = "X";
+
 	@Test
 	public void seCreaUnTableroVacio() {
 
@@ -46,7 +51,7 @@ public class TableroTest {
 
 		boolean posicionLibre = true;
 		Tablero tablero = new Tablero();
-		tablero.setPosicion(1, 3, "X");
+		tablero.setPosicion(1, 3, FICHA_JUGADOR_DOS);
 
 		String posicion = tablero.getPosicion(1, 3);
 		if (posicion != null) {
@@ -54,6 +59,16 @@ public class TableroTest {
 		}
 
 		Assert.assertTrue(!posicionLibre);
-		Assert.assertTrue(tablero.getPosicion(1, 3) == "X");
+		Assert.assertTrue(tablero.getPosicion(1, 3) == FICHA_JUGADOR_DOS);
+	}
+
+	@Test
+	public void elJugador1HaceSuPrimerJugadaEnLaColumna5() {
+
+		Tablero tablero = new Tablero();
+
+		tablero.jugar(JUGADOR_UNO, 5);
+
+		Assert.assertTrue(tablero.getPosicion(5, 4) == FICHA_JUGADOR_UNO);
 	}
 }
