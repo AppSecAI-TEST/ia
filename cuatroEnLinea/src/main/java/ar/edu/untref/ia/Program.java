762bcc46fd7718329086877fd56eed5108a51d0e
lcampaner <lucas.campaner@grupoesfera.com.ar>
2017-03-29 16:58:40 -0300

    Agrega menu.

--- cuatroEnLinea/src/main/java/ar/edu/untref/ia/Program.java
+++ cuatroEnLinea/src/main/java/ar/edu/untref/ia/Program.java
@@ -1,9 +1,50 @@
 package ar.edu.untref.ia;
 
-public class Program 
-{
-    public static final void main(String arg[])
-    {
-        
-    }
+import java.util.Scanner;
+
+public class Program {
+
+	private static Scanner scanner;
+
+	public static void main(String[] args) {
+
+		scanner = new Scanner(System.in);
+		int opcionSeleccionada = 0;
+
+		while (opcionSeleccionada != 3) {
+
+			visualizarMenu();
+			opcionSeleccionada = Integer.parseInt(scanner.nextLine());
+
+			switch (opcionSeleccionada) {
+			case 1:
+				System.out.println("Eligió la opcion 1");
+				break;
+			case 2:
+				System.out.println("Eligió la opcion 2");
+				break;
+			case 3:
+				break;
+			default:
+				System.out.println("Debe seleccionar una opción valida.");
+				break;
+			}
+		}
+	}
+
+	private static void visualizarMenu() {
+
+		System.out.println("*******************");
+		System.out.println("CUATRO EN LINEA!!!!");
+		System.out.println("*******************");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("1 - 1 VS 1");
+		System.out.println("2 - 1 VS PC");
+		System.out.println("");
+		System.out.println("3 - SALIR");
+		System.out.println("");
+		System.out.println("*******************");
+		System.out.println("Ingrese la opción:");
+	}
 }
