8385ddca700c522804c280a4973cda46ac08a164
Lucas <lucasdcampaner@gmail.com>
2017-04-16 19:35:12 -0300

    Correcion de error del merge.

--- cuatroEnLinea/src/test/java/ar/edu/untref/ia/TableroTest.java
+++ cuatroEnLinea/src/test/java/ar/edu/untref/ia/TableroTest.java
@@ -338,8 +338,5 @@ public class TableroTest {
 
 		// Comprobacion
 		Assert.assertTrue(tablero.getPosicion(2, 1 - 1).getContenido() == FICHA_JUGADOR_DOS);
-		tablero.jugarPC(tablero);
-
-		Assert.assertTrue(tablero.getPosicion(5, 6 - 1).getContenido() == FICHA_JUGADOR_DOS);
 	}
 }
