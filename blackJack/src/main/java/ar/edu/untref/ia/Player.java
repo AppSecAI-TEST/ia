3d47087fed98de89cfba764d9c25d75b20990839
Gonzalo Cozzi <gonzalocozzi@outlook.com>
2017-06-06 11:00:52 -0300

    [ADD] Player starts with an empty hand of cards.

--- blackJack/src/main/java/ar/edu/untref/ia/Player.java
+++ blackJack/src/main/java/ar/edu/untref/ia/Player.java
@@ -1,17 +1,26 @@
 package ar.edu.untref.ia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gonzalo on 06/06/17.
  */
 public class Player {
 
     private int points;
+    private List<String> hand;
 
     public Player(){
         this.points = 0;
+        this.hand = new ArrayList<>();
     }
 
     public int getPoints(){
         return this.points;
+    }
+
+    public List<String> getHand(){
+        return this.hand;
     }
 }
--- blackJack/src/test/java/ar/edu/untref/ia/PlayerTest.java
+++ blackJack/src/test/java/ar/edu/untref/ia/PlayerTest.java
@@ -3,6 +3,8 @@ package ar.edu.untref.ia;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * Created by gonzalocozzi on 06/06/17.
  */
@@ -15,6 +17,15 @@ public class PlayerTest {
         int initialPoints = player.getPoints();
 
         Assert.assertEquals(0, initialPoints);
+    }
+
+    @Test
+    public void playerStartsWithEmptyHandOfCards() throws Exception{
+
+        Player player = new Player();
+        List<String> hand = player.getHand();
+
+        Assert.assertTrue(hand.isEmpty());
     }
 
 }
