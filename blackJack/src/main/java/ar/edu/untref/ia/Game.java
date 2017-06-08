8363e476c2b3564b511b94362b56f6b67b0fe57d
Lucas <lucasdcampaner@gmail.com>
2017-06-08 11:40:22 -0300

    Formateo de codigo.

--- blackJack/src/main/java/ar/edu/untref/ia/Game.java
+++ blackJack/src/main/java/ar/edu/untref/ia/Game.java
@@ -8,107 +8,106 @@ import java.util.List;
  */
 public class Game {
 
-    private boolean isTerminal;
-    private Player player;
-    private Player croupier;
-    private List<Player> playersList;
-    private Deck deck;
-    private int playerPoints;
-    private int croupierPoints;
+	private boolean isTerminal;
+	private Player player;
+	private Player croupier;
+	private List<Player> playersList;
+	private Deck deck;
+	private int playerPoints;
+	private int croupierPoints;
 
-    public Game() {
-        this.isTerminal = false;
-        this.player = new Player();
-        this.croupier = new Player();
-        this.deck = new Deck();
-        this.playersList = new ArrayList<>();
-        this.playerPoints = 0;
-        this.croupierPoints = 0;
-        this.fillPlayersList();
-        this.firstStep();
-    }
+	public Game() {
+		this.isTerminal = false;
+		this.player = new Player();
+		this.croupier = new Player();
+		this.deck = new Deck();
+		this.playersList = new ArrayList<>();
+		this.playerPoints = 0;
+		this.croupierPoints = 0;
+		fillPlayersList();
+		firstStep();
+	}
 
-    private void fillPlayersList() {
-        this.playersList.add(this.player);
-        this.playersList.add(this.croupier);
-    }
+	private void fillPlayersList() {
+		this.playersList.add(this.player);
+		this.playersList.add(this.croupier);
+	}
 
-    private void firstStep() {
-        this.player.draw(this.deck);
-        this.croupier.draw(this.deck);
-        this.updatePlayersPoints();
-    }
+	private void firstStep() {
+		this.player.draw(this.deck);
+		this.croupier.draw(this.deck);
+		updatePlayersPoints();
+	}
 
-    private void updatePlayersPoints() {
-        this.playerPoints = this.player.getPoints();
-        this.croupierPoints = this.croupier.getPoints();
-    }
+	private void updatePlayersPoints() {
+		this.playerPoints = this.player.getPoints();
+		this.croupierPoints = this.croupier.getPoints();
+	}
 
-    protected boolean isTerminal() {
-        return this.isTerminal;
-    }
+	protected boolean isTerminal() {
+		return this.isTerminal;
+	}
 
-    protected Player getPlayer() {
-        return this.player;
-    }
+	protected Player getPlayer() {
+		return this.player;
+	}
 
-    protected Player getCroupier() {
-        return this.croupier;
-    }
+	protected Player getCroupier() {
+		return this.croupier;
+	}
 
-    protected List<Player> getPlayersList() {
-        return this.playersList;
-    }
+	protected List<Player> getPlayersList() {
+		return this.playersList;
+	}
 
-    protected Deck getDeck() {
-        return this.deck;
-    }
+	protected Deck getDeck() {
+		return this.deck;
+	}
 
-    protected int getPlayerPoints() {
-        return this.playerPoints;
-    }
+	protected int getPlayerPoints() {
+		return this.playerPoints;
+	}
 
-    protected int getCroupierPoints() {
-        return this.croupierPoints;
-    }
+	protected int getCroupierPoints() {
+		return this.croupierPoints;
+	}
 
-    protected int step(Action action) {
-        if(action == Action.DRAW){
-            this.player.draw(this.deck);
-            this.updatePlayersPoints();
-        } else if(action == Action.STAND){
-            this.isTerminal = true;
-            while(this.croupierPoints < 17){
-                this.croupier.draw(this.deck);
-                this.updatePlayersPoints();
-            }
-        }
-        return this.calculatePlayerReward();
-    }
+	protected int step(Action action) {
+		if (action == Action.DRAW) {
+			this.player.draw(this.deck);
+			this.updatePlayersPoints();
+		} else if (action == Action.STAND) {
+			this.isTerminal = true;
+			while (this.croupierPoints < 17) {
+				this.croupier.draw(this.deck);
+				this.updatePlayersPoints();
+			}
+		}
+		return this.calculatePlayerReward();
+	}
 
-    /**
-     * reward 1: player wins
-     * reward 0: tie
-     * reward -1: croupier wins
-     * @return reward
-     */
-    private int calculatePlayerReward(){
-        int reward = 0;
-        if(this.playerPoints > 21){
-            this.isTerminal = true;
-            reward = -1;
-        }
-        if(this.croupierPoints > 21){
-            if(this.playerPoints <= 21){
-                reward = 1;
-            }
-        } else if (this.playerPoints < this.croupierPoints) {
-            reward = -1;
-        } else if (this.playerPoints == this.croupierPoints) {
-            reward = 0;
-        } else if (this.playerPoints > this.croupierPoints) {
-            reward = 1;
-        }
-        return reward;
-    }
+	/**
+	 * reward 1: player wins reward 0: tie reward -1: croupier wins
+	 *
+	 * @return reward
+	 */
+	private int calculatePlayerReward() {
+		int reward = 0;
+		if (this.playerPoints > 21) {
+			this.isTerminal = true;
+			reward = -1;
+		}
+		if (this.croupierPoints > 21) {
+			if (this.playerPoints <= 21) {
+				reward = 1;
+			}
+		} else if (this.playerPoints < this.croupierPoints) {
+			reward = -1;
+		} else if (this.playerPoints == this.croupierPoints) {
+			reward = 0;
+		} else if (this.playerPoints > this.croupierPoints) {
+			reward = 1;
+		}
+		return reward;
+	}
 }
